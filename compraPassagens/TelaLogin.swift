63bbabdd132862c3b88d5c2c2762e81bbdebdb9d
01lucasPinheiro <pinheirolucas345543@gmail.com>
2026-04-09 15:00:35 -0300

    add componentes

--- compraPassagens/TelaLogin.swift
+++ compraPassagens/TelaLogin.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct TelaLogin: View {
+    @State private var email: String = ""
+    @State private var senha: String = ""
+    
     var body: some View {
         ZStack{
             VStack(spacing: 0){
@@ -24,47 +27,29 @@ struct TelaLogin: View {
                             .background(Color.blue)
                             .cornerRadius(10)
                         Spacer()
-                        Text("Cadastre-se")
-                            .frame(maxWidth: 99, maxHeight: 32)
-                            .background(Color.blue)
-                            .cornerRadius(10)
+                        NavigationLink(destination: telaCadastro()){
+                            Text("Cadastre-se")
+                                .foregroundStyle(Color.black)
+                                .frame(maxWidth: 99, maxHeight: 32)
+                                .background(Color.blue.opacity(0.6))
+                                .cornerRadius(10)
+                        }
                         Spacer()
                     }
                 }
-                VStack{
                     VStack{
-                        HStack{
-                            Text("E-MAIL")
-                                .font(Font.system(size: 12, weight: .bold, design: .default))
-                            Spacer()
-                        }
-                        TextField("Digite seu email", text: .constant(""))
-                    }
-                    .padding()
-                        .frame(maxWidth: 300, maxHeight: 60)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 1))
-                    VStack{
-                        HStack{
-                            Text("SENHA")
-                                .font(Font.system(size: 12, weight: .bold, design: .default))
-                            Spacer()
-                        }
-                        TextField("************", text: .constant(""))
-                    }
-                    .padding()
-                        .frame(maxWidth: 300, maxHeight: 60)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 1))
+                        CustomInputField(label: "E-MAIL", placeholder: "Digite seu email", text: $email)
+                        CustomInputField(label: "SENHA", placeholder: "*********", text: $senha)
                 }.padding(20)
                 VStack{
                 Text("Esqueceu a senha")
                     .underline()
                     .foregroundStyle(Color.orange)
-
-                    Text("Avançar")
-                        .foregroundColor(.white) // Garante que o texto não fique no azul padrão de links
-                        .frame(width: 179, height: 34)
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                        Text("Avançar")
+                            .foregroundColor(.white) // texto não fique no azul padrão de links
+                            .frame(width: 179, height: 34)
+                            .background(Color.blue)
+                            .cornerRadius(10)
                 }
             Spacer()
                 }
